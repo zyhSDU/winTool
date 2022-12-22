d50b2a35f7b4efc139a83a48b484705c958e396a
zyhSDU <34438397+zyhSDU@users.noreply.github.com>
2022-12-22 17:45:59 +0800

    ObjectHelper.py

--- helper/ObjectHelper.py
+++ helper/ObjectHelper.py
@@ -1,7 +1,16 @@
+def get_object_str(self):
+    f"{type(self).__name__}("
+    f"{', '.join('%s=%s' % item for item in vars(self).items())}"
+    f")"
+
+
 def print_object(self, file=None):
     print(
-        f"{type(self).__name__}("
-        f"{', '.join('%s=%s' % item for item in vars(self).items())}"
-        f")",
+        get_object_str(self),
         file=file,
     )
+
+
+class Object(object):
+    def __str__(self):
+        return get_object_str(self)
